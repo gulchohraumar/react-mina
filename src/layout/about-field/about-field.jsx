7b094b36ba6e7a1d0a40c72ce3ed5469c9c4e412
Gulcohre Umarli <gulchohreum20631@sabah.edu.az>
2023-11-27 23:42:25 +0400

    ui

--- src/layout/about-field/about-field.jsx
+++ src/layout/about-field/about-field.jsx
@@ -2,13 +2,13 @@ import './about-field.scss'
 
 export default function AboutField() {
     return <>
-        <div className='mt-5 bg-danger about-field-container'>
+        <div className='py-5 bg-danger about-field-container'>
             <div className="row px-5">
-                <div className="col-md-6">
+                <div className="col-md-6 about-field-item">
                     <div className="col-md-12 about-img">
                         <img src={require('../../assets/mina.avif')} alt="" />
                     </div>
-                    <div className="about-content">
+                    <div className="mt-4 about-content">
                         <h2>About Mina</h2>
                     </div>
                     <p className='semi-header'>Mina is a brand born out of a passion for ceramics.</p>
